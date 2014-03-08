4bd49f3cf1529f6116d4326f2ded7ac3a7f9bf0a
daliborn <s2-roo@vmware.com>
2014-03-08 15:58:57 +0100

    entity jpa --class ~.domain.security.SecurityRole --testAutomatically

---
+++ src/test/java/info/share/portal/domain/security/SecurityRoleDataOnDemand.java
@@ -0,0 +1,6 @@
+package info.share.portal.domain.security;
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = SecurityRole.class)
+public class SecurityRoleDataOnDemand {
+}
---
+++ src/test/java/info/share/portal/domain/security/SecurityRoleIntegrationTest.java
@@ -0,0 +1,11 @@
+package info.share.portal.domain.security;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = SecurityRole.class)
+public class SecurityRoleIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
